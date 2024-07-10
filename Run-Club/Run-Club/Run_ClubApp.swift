3ea79e849921afc40fe28c11b92ca8b983abc6e0
Meghan Murphy <meghanmurphy@meghans-mbp.mynetworksettings.com>
2024-07-10 16:20:41 -0400

    blank react native app

---
+++ Run-Club/Run-Club/Run_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_ClubApp.swift
+//  Run-Club
+//
+//  Created by Meghan Murphy on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Run_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
